56a044d8b0986bc0ffa47a3e253bac411c0eb88a
unknown <contacto.acero.hernan@gmail.com>
2022-01-03 23:44:10 -0500

    get_products_by_collection added

--- src/controllers/CollectionController.js
+++ src/controllers/CollectionController.js
@@ -9,6 +9,28 @@ const getAllCollections = (req, res) => {
   });
 };
 
+const getCollectionByCategory = (req, res) => {
+  const { category } = req.params;
+
+  CollectionModel.find({ category }, (err, collection) => {
+    if (err) res.status(400).send(err.message);
+
+    res.status(200).json(collection);
+  });
+};
+
+const getProductsByCategory = (req, res) => {
+  const { category } = req.params;
+
+  CollectionModel.find({ category }, (err, collection) => {
+    if (err) res.status(400).send(err.message);
+
+    const { products } = collection[0];
+
+    res.status(200).json(products);
+  });
+};
+
 const getProductById = (req, res) => {
   const { category, id } = req.params;
   CollectionModel.find({ category }, (err, data) => {
@@ -34,4 +56,10 @@ const addCollection = (req, res) => {
   });
 };
 
-module.exports = { getAllCollections, getProductById, addCollection };
+module.exports = {
+  getAllCollections,
+  getProductsByCategory,
+  getCollectionByCategory,
+  getProductById,
+  addCollection,
+};
--- src/routes/collections.js
+++ src/routes/collections.js
@@ -3,6 +3,8 @@ const CollectionController = require("../controllers/CollectionController");
 const router = express.Router();
 
 router.get("/", CollectionController.getAllCollections);
+router.get("/:category", CollectionController.getCollectionByCategory);
+router.get("/:category/products", CollectionController.getProductsByCategory);
 router.get("/:category/:id", CollectionController.getProductById);
 router.post("/", CollectionController.addCollection);
 
